e61f478eebf30321461b1e111bde062449481072
Jacin <jacinjiyan@gmail.com>
2021-03-25 13:50:17 +1100

    added basic register styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
         <nav className="navbar is-warning is-transparent">
                 <div className="navbar-brand ">
                     <Link className="navbar-item" to="/" style={{padding: 0}}>
-                        <img src="assets/logo.png" alt="" style={{ maxHeight: 85 }} />
+                        <img src="assets/logo.png" alt="" style={{ maxHeight: 80 }} />
                         <span style={{paddingLeft: 20}}><strong>BOOK WITH US!</strong></span>
                     </Link>    
                     <Link to="#" className="navbar-burger" >
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 const Register = () => {
     const [username, setUsername] = useState('')
@@ -9,16 +10,48 @@ const Register = () => {
         alert('infro')
     }
 
-
     return (
-        <div className="hero is-primary is-fullheight">
+        <div className="hero is-primary is-fullheight-with-navbar">
             <div className="hero-body">
                 <div className="container has-text-centered">
                     <div className="columns">
                         <div className="column is-8 is-offset-2">
-                            <h3 className="title has-text-white">Login</h3>
+                            <h3 className="title has-text-white">Sign Up</h3>
                             <hr className="login-hr" />
-                            <p className="subtitle has-text-white">Please login to start booking!</p>
+                            <p className="subtitle has-text-white">Start the booking with us today!</p>
+                            <div className="box">
+                                <form>
+                                    <div className="field">
+                                        <div className="control">
+                                            <input className="input" type="text" placeholder="Username" autoFocus="" />
+                                        </div>
+                                    </div>
+                                    <div className="field">
+                                        <div className="control">
+                                            <input className="input" type="email" placeholder="Email" autoFocus="" />
+                                        </div>
+                                    </div>
+                                    <div className="field">
+                                        <div className="control">
+                                            <input className="input " type="password" placeholder="Password" />
+                                        </div>
+                                    </div>
+                                    <div className="field">
+                                        <div className="control">
+                                            <input className="input " type="password" placeholder="Confirm Password" />
+                                        </div>
+                                    </div>
+                                    <label className="checkbox" style={{ margin: 20 }}>
+                                        <input type="checkbox" /> Remember me
+                                    </label>
+                                    <button className="button is-block is-warning is-fullwidth">Login
+                                    </button>
+                                </form>
+                            </div>
+                            <p className="has-text-white">
+                                <Link to="">Sign In</Link> &nbsp;·&nbsp;
+                                <Link to="">Need Help?</Link>
+                            </p>
                         </div>
                     </div>
                 </div>
